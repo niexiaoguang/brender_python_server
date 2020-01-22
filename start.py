542b245bc99417c0dbb00b1feca8b2678bfe54f9
niexiaoguang <nxg@lohosoft.com>
2020-01-22 14:12:40 +0800

    make subprocess output std and stderr into into job.meta['output']

--- start.py
+++ start.py
@@ -1,12 +1,22 @@
 from rq import Queue
 from worker import conn
+import time
 
 from task import count_words_at_url
-from task import oscmd
-
+# from task import oscmd
+from task import render
+from task import updateJobMeta
 q = Queue(connection=conn)
 
-# result = q.enqueue(count_words_at_url, 'http://www.heroku.com')
-result = q.enqueue(oscmd)
+jobID = 'myJobId'
+# result = q.enqueue(count_words_at_url, 'http://www.heroku.com',job_id=jobID)
+# result = q.enqueue(oscmd)
 
-print(result)
+job = q.enqueue(render,job_id=jobID)
+job.meta['output'] = 'init'
+job.save_meta()
+
+
+while True:
+	time.sleep(2)
+	updateJobMeta(jobID)
